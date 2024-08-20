5ab63fb7e3bee88e8ec5fa76e79baa770828221b
KarenCagape <karenjoycagape@gmail.com>
2024-08-20 23:15:47 +0800

    test output

--- functions/update-company.js
+++ functions/update-company.js
@@ -7,33 +7,35 @@ exports.handler = async function (event, context) {
   const data = JSON.parse(body);
 
   /* const values = {
-    multiselect: [
-      {
-        id: "1",
-        name: "Option 1",
-        type: "option",
-        order: 0,
-      },
-      {
-        id: "2",
-        name: "Option 2",
-        type: "option",
-        order: 1,
-      },
-    ],
-    text_column: "sample text value",
-    number_column: 76,
-  };
-  const HubDbTableRowV3Request = { values };
-  const tableIdOrName = "18579329";
-  const rowId = "rowId";
+  multiselect: [
+    {
+      id: "1",
+      name: "Option 1",
+      type: "option",
+      order: 0,
+    },
+    {
+      id: "2",
+      name: "Option 2",
+      type: "option",
+      order: 1,
+    },
+  ],
+  text_column: "sample text value",
+  number_column: 76,
+};
+const HubDbTableRowV3Request = { values };
+const tableIdOrName = "18579329";
+const rowId = "rowId";
 
-  try {
-    const apiResponse = await hubspotClient.cms.hubdb.rowsApi.updateDraftTableRow(tableIdOrName, rowId, HubDbTableRowV3Request);
-    console.log(JSON.stringify(apiResponse, null, 2));
-  } catch (e) {
-    e.message === "HTTP request failed" ? console.error(JSON.stringify(e.response, null, 2)) : console.error(e);
-  } */
+try {
+  const apiResponse = await hubspotClient.cms.hubdb.rowsApi.updateDraftTableRow(tableIdOrName, rowId, HubDbTableRowV3Request);
+  console.log(JSON.stringify(apiResponse, null, 2));
+} catch (e) {
+  e.message === "HTTP request failed" ? console.error(JSON.stringify(e.response, null, 2)) : console.error(e);
+} */
+
+  console.log(data);
 
   return {
     body: JSON.stringify(data),
