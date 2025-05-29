2cbc7ec12347d1471c70439207280f527579d109
KarenCagape <karenjoycagape@gmail.com>
2025-05-30 01:47:37 +0800

    used @sentry/node@7.92.0

--- functions/update-company.js
+++ functions/update-company.js
@@ -2,11 +2,7 @@ const Sentry = require("@sentry/node");
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
+  tracesSampleRate: 1.0,
   sendDefaultPii: true,
 });
 
