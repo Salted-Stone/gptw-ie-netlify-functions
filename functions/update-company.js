deb6aa9a346663f16378f6788c3079e39dc08ff8
KarenCagape <karenjoycagape@gmail.com>
2025-05-30 00:37:47 +0800

    added sentry

--- functions/update-company.js
+++ functions/update-company.js
@@ -1,3 +1,21 @@
+const Sentry = require("@sentry/aws-serverless");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
+  profileLifecycle: "trace",
+
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
 const hubspot = require("@hubspot/api-client");
 const axios = require("axios");
 const FormData = require("form-data");
@@ -5,41 +23,50 @@ const FormData = require("form-data");
 const hubspotClient = new hubspot.Client({ accessToken: process.env.HS_API_TOKEN });
 
 const uploadFile = async (file, fileName) => {
-  const form = new FormData();
+  // Start a Sentry span for the uploadFile operation
+  const span = Sentry.startSpan({ op: "uploadFile", description: "Uploading file to HubSpot" });
 
+  const form = new FormData();
   const fileOptions = {
     access: "PUBLIC_NOT_INDEXABLE",
   };
 
-  const buffer = Buffer.from(file.split(",")[1], "base64");
-
-  form.append("file", buffer, { filename: fileName });
-  form.append("options", JSON.stringify(fileOptions));
-  form.append("folderId", process.env.COMPANY_UPLOADS_FOLDER_ID);
-
-  const config = {
-    method: "post",
-    maxBodyLength: Infinity,
-    url: "https://api.hubapi.com/files/v3/files",
-    headers: {
-      Authorization: `Bearer ${process.env.HS_API_TOKEN}`,
-      ...form.getHeaders(),
-    },
-    data: form,
-  };
-
   try {
-    const res = await axios(config);
+    if (!file || !file.includes(",")) {
+      throw new Error("Invalid file format");
+    }
+    
+    const buffer = Buffer.from(file.split(",")[1], "base64");
 
+    form.append("file", buffer, { filename: fileName });
+    form.append("options", JSON.stringify(fileOptions));
+    form.append("folderId", process.env.COMPANY_UPLOADS_FOLDER_ID);
+
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://api.hubapi.com/files/v3/files",
+      headers: {
+        Authorization: `Bearer ${process.env.HS_API_TOKEN}`,
+        ...form.getHeaders(),
+      },
+      data: form,
+    };
+    const res = await axios(config);
     return res?.data;
   } catch (e) {
     e.message === "HTTP request failed" ? console.error(JSON.stringify(e.response, null, 2)) : console.error(e);
+    // Optionally capture the exception in Sentry
+    Sentry.captureException(e);
+  } finally {
+    // End the Sentry span
+    span.end();
   }
 };
 
 async function* asyncIterable(data) {
   const keys = Object.keys(data);
-  const values = new Object();
+  const values = {};
   const numFields = ["statistic", "tenure_2years", "tenure_6_10years", "tenure_11_15years", "tenure_16_20years", "tenure_over_20years"];
 
   for (const key of keys) {
@@ -80,51 +107,59 @@ async function* asyncIterable(data) {
         setVal = values["industry"];
         break;
       case "company_logo":
-        const logo = await uploadFile(data["company_logo"].file, data["company_logo"].file_name);
-        if (logo) {
-          values["company_logo"] = {
-            url: logo.url,
-            width: logo.width,
-            height: logo.height,
-            type: "image",
-          };
-          setVal = values["company_logo"];
+        if (data["company_logo"]?.file && data["company_logo"]?.file_name) {
+          const logo = await uploadFile(data["company_logo"].file, data["company_logo"].file_name);
+          if (logo) {
+            values["company_logo"] = {
+              url: logo.url,
+              width: logo.width,
+              height: logo.height,
+              type: "image",
+            };
+            setVal = values["company_logo"];
+          }
         }
         break;
       case "featured_thumbnail":
-        const thumb = await uploadFile(data["featured_thumbnail"].file, data["featured_thumbnail"].file_name);
-        if (thumb) {
-          values["featured_thumbnail"] = {
-            url: thumb.url,
-            width: thumb.width,
-            height: thumb.height,
-            type: "image",
-          };
-          setVal = values["featured_thumbnail"];
+        if (data["featured_thumbnail"]?.file && data["featured_thumbnail"]?.file_name) {
+          const thumb = await uploadFile(data["featured_thumbnail"].file, data["featured_thumbnail"].file_name);
+          if (thumb) {
+            values["featured_thumbnail"] = {
+              url: thumb.url,
+              width: thumb.width,
+              height: thumb.height,
+              type: "image",
+            };
+            setVal = values["featured_thumbnail"];
+          }
         }
         break;
       case "word_from_the_top_image":
-        const topImage = await uploadFile(data["word_from_the_top_image"].file, data["word_from_the_top_image"].file_name);
-        if (topImage) {
-          values["word_from_the_top_image"] = {
-            url: topImage.url,
-            width: topImage.width,
-            height: topImage.height,
-            type: "image",
-          };
-          setVal = values["word_from_the_top_image"];
+        if (data["word_from_the_top_image"]?.file && data["word_from_the_top_image"]?.file_name) {
+          const topImage = await uploadFile(data["word_from_the_top_image"].file, data["word_from_the_top_image"].file_name);
+          if (topImage) {
+            values["word_from_the_top_image"] = {
+              url: topImage.url,
+              width: topImage.width,
+              height: topImage.height,
+              type: "image",
+            };
+            setVal = values["word_from_the_top_image"];
+          }
         }
         break;
       case "culture_overview_image":
-        const cultureImage = await uploadFile(data["culture_overview_image"].file, data["culture_overview_image"].file_name);
-        if (cultureImage) {
-          values["culture_overview_image"] = {
-            url: cultureImage.url,
-            width: cultureImage.width,
-            height: cultureImage.height,
-            type: "image",
-          };
-          setVal = values["culture_overview_image"];
+        if (data["culture_overview_image"]?.file && data["culture_overview_image"]?.file_name) {
+          const cultureImage = await uploadFile(data["culture_overview_image"].file, data["culture_overview_image"].file_name);
+          if (cultureImage) {
+            values["culture_overview_image"] = {
+              url: cultureImage.url,
+              width: cultureImage.width,
+              height: cultureImage.height,
+              type: "image",
+            };
+            setVal = values["culture_overview_image"];
+          }
         }
         break;
       default:
@@ -160,9 +195,9 @@ const saveLog = async function (values, rowName) {
   }
 };
 
-exports.handler = async function (event, context) {
+exports.handler = Sentry.wrapHandler(async (event, context) => {
   const { body, httpMethod } = event;
-  const dateNow = new Date();
+  // const dateNow = new Date();
   const valuesLogs = { status: "", payload: "", date: Date.now(), error_code: "" };
 
   valuesLogs.payload = JSON.parse(body);
@@ -181,7 +216,7 @@ exports.handler = async function (event, context) {
   }
 
   valuesLogs.payload = JSON.stringify(valuesLogs.payload);
-  console.log(valuesLogs);
+  // console.log(valuesLogs);
 
   let headers = {
     "Access-Control-Allow-Origin": "*",
@@ -194,8 +229,8 @@ exports.handler = async function (event, context) {
     const email = data?.email;
     const companyName = data?.name;
 
-    const getAllVAlues = async () => {
-      const allValues = new Object();
+    const getAllValues = async () => {
+      const allValues = {};
 
       for await (const res of asyncIterable(data)) {
         // console.log(res);
@@ -207,7 +242,9 @@ exports.handler = async function (event, context) {
       return allValues;
     };
 
-    const values = await getAllVAlues();
+    const spanPrepare = Sentry.startSpan({ op: "prepareData", description: "Preparing all values" });
+    const values = await getAllValues();
+    spanPrepare.end();
 
     let query = new URLSearchParams();
 
@@ -236,10 +273,14 @@ exports.handler = async function (event, context) {
           const company = json.results[0];
           const HubDbTableRowV3Request = { values };
 
+          const spanUpdate = Sentry.startSpan({ op: "hubdb.updateRow", description: "Updating HubDB row" });
           const updateRow = await hubspotClient.cms.hubdb.rowsApi.updateDraftTableRow(tableIdOrName, company.id, HubDbTableRowV3Request);
-          console.log(JSON.stringify(values, null, 2));
+          spanUpdate.end();
+          // console.log(JSON.stringify(values, null, 2));
 
+          const spanPublish = Sentry.startSpan({ op: "hubdb.publishTable", description: "Publishing HubDB table" });
           const publishTable = await hubspotClient.cms.hubdb.tablesApi.publishDraftTable(tableIdOrName);
+          spanPublish.end();
 
           valuesLogs.status = {
             name: "success",
@@ -287,13 +328,13 @@ exports.handler = async function (event, context) {
         name: "error",
         type: "option",
       };
-      valuesLogs.error_code = "404";
+      valuesLogs.error_code = "400";
 
       await saveLog(valuesLogs, companyName);
 
       return {
         headers,
-        statusCode: 404,
+        statusCode: 400,
       };
     }
   } else {
@@ -310,4 +351,4 @@ exports.handler = async function (event, context) {
       statusCode: 405,
     };
   }
-};
+});
